593e422a16f35f4e5ee606d9981114dd8d7ca76f
kavs1346 <130669072+kavs1346@users.noreply.github.com>
2023-10-17 16:26:19 -0300

    - commit estações do ano

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
-# Não fiz bem detalhado!!
-dia = int(input('Digite o dia: '))
+#Sem detalhes!!
 mes = int(input('Digite o mês: '))
 
-if mes >= 1 and mes <= 3:
-    print('Verão')
-elif mes > 3 and mes <= 6:
-    print('Outono')
-elif mes > 6 and mes <= 9:
-    print('Inverno')
-elif mes == 12:
-    print('Verão')
+if mes <= 12:
+    if mes >= 1 and mes <= 3:
+        print('Verão')
+    elif mes > 3 and mes <= 6:
+        print('Outono')
+    elif mes > 6 and mes <= 9:
+        print('Inverno')
+    elif mes == 12:
+        print('Verão')
+    else:
+        print('Primavera')
 else:
-    print('Primavera')
+    print('Mes invalido')
